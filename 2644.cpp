46bd8fe2eacd5de28c344c287dfaf71775e6e523
noahimhere <noahimhere@gmail.com>
2023-04-22 16:26:54 +0900

    yes

--- 2644.cpp
+++ 2644.cpp
@@ -1,119 +1,60 @@
 #include <bits/stdc++.h>
-#include <string>
 
 using namespace std;
 
+
+vector<int> relation[101];
+
 int n;
-int m;
-int temp1;
-int temp2;
-vector<pair<int, int>> relation;
 
-int starting;
-
-bool beento[101];
-
-int currentcounting = 0;
-int maxi = 0;
-
-int fos = 5;
-
-int qsize = 0;
 int p;
 int c;
-int e;
-
-int chonsu;
+int m;
 
 
+bool visited[101];
 
+int bfs(int parent, int child) {
+    queue<pair<int, int>> q;
+    visited[parent] = true;
+    q.push({parent, 0});
 
-
-
-
-
-
-void bfs(int startc){
-    queue<pair<int ,int>> q;
-    q.push(relation[starting]);
-    chonsu++;
-    
-
-    while(!q.empty()){
-        pair<int, int> comp = q.front();
+    while (!q.empty()) {
+        int qfir = q.front().first;
+        int qscnd = q.front().second;
         q.pop();
 
-        
-        for(int i = 1; i < relation.size(); i++){
-            if(relation[i].first == p || relation[i].second == p){
-                cout << chonsu;
-                return;
-            }
-            else if(fos == 0){
-                if(relation[i].first == comp.first && i != starting){
-                    q.push(relation[i]);
-                    beento[i] == true;
-                    chonsu++;
-                }
-                if(relation[i].first == comp.second && i != starting){
-                    q.push(relation[i]);
-                    beento[i] == true;
-                    chonsu++;
-                }
-            }
-            else{
-                if(relation[i].second == comp.first && i != starting){
-                    q.push(relation[i]);
-                    beento[i] == true;
-                    chonsu++;
-                }
-                if(relation[i].second == comp.second && i != starting){
-                    q.push(relation[i]);
-                    beento[i] == true;
-                    chonsu++;
-                }
-            }
-
+        if (qfir == child) {
+            cout << qscnd;
+            return 0;
         }
 
-        
-            
-        
+        for (int i = 0; i < relation[qfir].size(); i++) {
+            int next = relation[qfir][i];
+            if (!visited[next]) {
+                visited[next] = true;
+                q.push({next, qscnd+1});
+            }
+        }
     }
-    cout << currentcounting;
 
-    
+    cout << -1;
+    return -1;
 }
 
+int main() {
+    cin >> n >> p >> c >> m;
 
-
-
-int main(){
-    cin >> n;
-    cin >> p >> c;
-    cin >> e;
-    for(int i = 0; i < e; i++){
+    for (int i = 0; i < m; i++) {
+        int temp1, temp2;
         cin >> temp1 >> temp2;
-        if(temp1 == c){
-            starting = i;
-            fos = 0;
-        }
-        if(temp2 == c){
-            starting = i;
-            fos = 1;
-        }
-        // cout << starting << "\n";
-        relation.push_back(make_pair(temp1, temp2));
+        relation[temp1].push_back(temp2);
+        relation[temp2].push_back(temp1);
     }
 
-    // cout << starting << " " << fos;
+    bfs(p,c);
 
-    if(fos == 5){
-        cout << -1;
-    }
-    bfs(1);
-
-
-    
-    
+    return 0;
 }
+
+
